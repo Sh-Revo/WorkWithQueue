2db0e279d634471110a3563a555e428e13259afa
Rostislav <shandov@i.ua>
2022-04-10 11:57:42 +0300

    add algorithm to search numbers of Fibonacci

--- WorkWithQueue/Program.cs
+++ WorkWithQueue/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace WorkWithQueue
 {
@@ -6,7 +9,34 @@ namespace WorkWithQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach (var e in Sequence.Fibonacci)
+            {
+                Console.WriteLine(e);
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                    break;
+            }
+        }
+    }
+
+    public class Sequence
+    {
+        public static IEnumerable<int> Fibonacci
+        {
+            get
+            {
+                int previous = 1;
+                int current = 1;
+                yield return 1;
+                yield return 1;
+                while (true)
+                {
+                    var newValue = current + previous;
+                    previous = current;
+                    current = newValue;
+                    yield return current;
+                }
+            }
         }
     }
 }
