de5e8eec3b4ac5272ae381a6cdd30fa44ea3bb3e
Rostislav <shandov@i.ua>
2022-04-10 12:20:32 +0300

    add Queue based on LilnkedList

--- WorkWithQueue/Program.cs
+++ WorkWithQueue/Program.cs
@@ -6,7 +6,61 @@ namespace WorkWithQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+    }
+
+    class QueueItem
+    {
+        public int value { get; set; }
+        public QueueItem Next { get; set; }
+    }
+
+    class Queue
+    {
+        QueueItem head;
+        QueueItem tail;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return head == null;
+            }
+        }
+
+        public void Enqueue(int value)
+        {
+            var item = new QueueItem
+            {
+                value = value
+            };
+        if (head == null)
+            {
+                head = tail = item;
+            }
+        else
+            {
+                tail.Next = item;
+                tail = item;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var result = head.value;
+            head = head.Next;
+            if (head == null)
+                tail = null;
+            return result;
         }
     }
 }
+
+
+
